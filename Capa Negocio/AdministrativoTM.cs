68056d4a22f3ac4091891784d8a515ab30744374
vicgolob <victorgolob@gmail.com>
2013-10-19 19:20:38 -0300

    Actualizacion 19/10

---
+++ Capa Negocio/AdministrativoTM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Clinica_frba.CapaNegocio
+{
+    class AdministrativoTM
+    {
+        // Atributos
+        CapaDatos.AdministrativoTDG admTDG; // Notar que es del tipo AdministrativoTDG, clase que se encuentra en la Capa de Datos
+                
+        // Constructor 
+        public AdministrativoTM() { admTDG = new CapaDatos.AdministrativoTDG(); }
+        
+        
+        // Métodos
+        public DataSet getAdmById(int id)
+        {
+            DataSet adm = admTDG.getAdmById(id);
+            return adm;
+        }
+
+
+        public DataSet getAdmByAtributos() { return new DataSet(); }
+       
+        
+        
+        public void insert() {}
+        
+
+
+        public void update() {
+            DataTable adm = new DataTable();
+
+            // Algún código obteniendo datos y metiendolos dentro del DataTable (es un objeto como cualquier otro)
+
+            admTDG.update(adm); // actualizo los datos      
+        }
+
+
+        public void delete() {} 
+    }
+}
